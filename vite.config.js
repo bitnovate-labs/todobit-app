20d8f17c7f4a42866cc17189972691562b204899
Timothy Lim <timlimytn@gmail.com>
2025-01-14 22:57:40 +0800

    updated index.html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+// Allows users to install your app on their homescreens
 const manifestForPlugin = {
   registerType: "autoUpdate",
   includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.png"],
