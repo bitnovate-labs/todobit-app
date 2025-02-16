a18cd9ef74fbf5d538ae3e6a24fe32f30f4216f6
Timothy Lim <timlimytn@gmail.com>
2025-02-16 09:51:11 +0800

    Updates

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig({
             src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "/pwa-512x512.png",
